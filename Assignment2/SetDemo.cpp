77cdf604303693db5296b626075f3ca9a860cf16
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-01-30 23:59:53 -0800

    fix input

--- Assignment2/SetDemo.cpp
+++ Assignment2/SetDemo.cpp
@@ -1,32 +1,42 @@
 #include<iostream>
 #include"MySet.h"
+#include<assert.h>
 using namespace std;
+#define REP(i,n) for(int i=0;i<int(n);i++)
 
-int getInput(int arr[], int max_sz){
-    char p;
-    int ind=0;
-    char prev;
-    int num=0;
-    while(cin.get(p) && --max_sz){
-        if(p=='\n')return ind+1;
-        if(p==' ')continue;
-        if(!isdigit(p)){
-            cout<<"invalid input"<<endl;
-            exit(1);
+int getInput(int arr[]){
+    char cur,prev;
+    int buff=0,ind=0;
+    while(cin.get(cur)){
+        if(cur=='\n'){
+            if(isdigit(prev)){
+                arr[ind]=buff;
+                ind++;
+            }
+            break;
         }
-        arr[ind]=p-'0';
-        ++ind;
+        if(cur==' '){
+            if(isdigit(prev)){
+                arr[ind]=buff;
+                ++ind,buff=0;
+                prev=' ';
+                assert(isdigit(prev)==false);
+            }
+            continue;
+        };
+        buff = 10*buff + (cur-'0');
+        prev = cur;
     }
-    return min((int)max_sz,ind+1);
+    return ind;
 }
 int main(){
     int first[20]={};
     int second[20]={};
     char p;
     cout<<"Input first list >> ";
-    int f_sz = getInput(first,20);
-    cout<<"Input first list >> ";
-    int s_sz = getInput(second,20);
+    int f_sz = getInput(first);
+    cout<<"Input second list >> ";
+    int s_sz = getInput(second);
 
     IntegerSet firstSet(first,f_sz);
     IntegerSet secondSet(second,f_sz);
