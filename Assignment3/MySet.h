4b1f8f58d0fd9b13c5adad93f167a8f689737795
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-02-08 21:45:31 -0800

    forgot to update

--- Assignment3/MySet.h
+++ Assignment3/MySet.h
@@ -1,39 +1,8 @@
-/* This class defines a set of integers called MySet. Objects of the type MySet are a set including distinct integers, if they are not empty.
-   Integers of the set may have any order and they may also change their order anytime if it helps implementing a more efficient function.
-   
-    This class includes three attributes:
-    1) set: A pointer to a sequence of integers stored in the heap.
-    2) size: An integer showing the number of integers included in the set.
-    3) bufferSize: An integer showing the size of the buffer (i.e. the maximum number of integers that fit in the set at any point).
-    The bufferSize is initially 10 and is increased by 50% if a single integer is about to be added to a full set. If more than one item is
-        to be added to a full set and 50% increase is not enough, the set is increased enough to accomodate all the new values.
-
-    This class also defines the following methods:
-    1) A no-arg constructor that creates an empty set with the buffer size of 10.
-    2) A parameterized constructor with two input parameters: an array of integers and the size of the array. This
-        method creates a set including the distinct integers in the input array. The buffer size in this case is identified as described for adding 
-        more than one value to the set.
-    3) A copy constructor that creates a MySet object using an existing MySet object.
-    4) The class destructor.
-    5) The "add" method that accepts an integer as its input parameter and if the input value does not exist in the set, adds it to the set and returns true; 
-        otherwise, it returns false and of course the value is not added to the set.
-    6) Overload the "add" function to all the distinct values of an array of integers to the set.
-    7) The "remove" method that accepts an integer as its input parameter and if the input value exists in the set, it is removed from the set, and
-        the function returns true; otherwise, it returns false.
-    8) The "has" method checks if the input parameter exists in the set. This method returns true if the input value is a member of the set 
-        and returns false otherwise.
-    9) The "unionWith" method creates and returns a new MySet object including all the integers of the current set and another set passed as function's input 
-        parameter.
-    10) The "intersectWith" method creates and returns the intersection of the set with another set passed in the function's input parameter.
-        The intersection of two sets is a set including all the items that commonly exist in both sets.
-    11) The "clear" method clears the set; empties the set and reset is to its initial state.
-    12) The "getSize" method returns the number of items included in the set.
-	13) The "print" method displays the set on the screen.
-   */
-
-#ifndef MY_SET_H
-#define MY_SET_H
-#include <set>//Only for a test function
+#include<iostream>
+#include<assert.h>
+#include<algorithm>
+// This is only for test
+#include <set>
 
 template<typename T>
 class MySet {
@@ -61,4 +30,207 @@ private:
     
     
 };
-#endif
+
+
+template<typename T>
+MySet<T>::MySet(){
+    set = new T[10];
+    size=0, bufferSize=10;
+    assert(bufferSize>=size);
+}
+
+template<typename T>
+MySet<T>::MySet(const T sequence[], int size){
+    this->set = new T[10];
+    this->size=0;
+    this->bufferSize=10;
+    add(sequence,size);
+    assert(bufferSize>=this->size);
+}
+
+template<typename T>
+MySet<T>::MySet(const MySet& anotherSet){
+    bufferSize = anotherSet.bufferSize;
+    size = anotherSet.size;
+    assert(bufferSize>=size);
+    set = new T[bufferSize];
+    for(int i=0;i<size;++i) set[i]=anotherSet.set[i];
+}
+
+
+template<typename T>
+MySet<T>::~MySet(){delete[] set;}
+
+
+//O(N)
+template<typename T>
+void MySet<T>::allocateNewBuffer(int new_size){
+    //allocate memory
+    T* new_set = new T[new_size];
+    int new_ind=0;
+    for(int old_ind=0;old_ind<size;++old_ind,++new_ind)
+        new_set[new_ind]=set[old_ind];
+    delete[] set, set = new_set;
+    bufferSize = new_size;
+}
+
+
+//O(N)
+template<typename T>
+bool MySet<T>::add(T item){
+    //After added, the set should be sorted;
+    assert(bufferSize>=size);
+    if(size==0){
+        set[0]=item, ++size;
+        return true;
+    }
+    if(has(item))return false;
+    if(bufferSize==size) allocateNewBuffer(bufferSize+bufferSize/2);
+
+    //add to the tail
+    assert(size-1>=0);
+    if(set[size-1]<item){
+        set[size]=item, ++size;
+        return true;
+    }
+    //insert to middle
+    //binary search
+    int ok = size-1, ng = -1;
+    assert(item<set[ok]);
+    while(ok-ng>1){
+        int mid = (ok+ng)/2; assert(mid>=0);
+        if(set[mid]>item)ok=mid;
+        else ng = mid;
+        assert(item<set[ok]);
+    }
+    assert(ok-1 < 0 || item > set[ok-1]);
+    /*
+        Shift to right
+            a, b, ok, c, d
+    =>      a, b, new,ok,c,d
+        set[size-1]=>set[size], set[ok]=>set[ok+1];
+    */
+    //Shift to right
+    for(int i=size-1;i>=ok;--i) set[i+1] = set[i];
+    set[ok]=item, ++size;
+    return true;
+}
+
+
+//O( Nlog(N))
+template<typename T>
+bool MySet<T>::add(const T sequence[], int size){
+    assert(bufferSize >= this->size);
+    int all_size = size + this->size;
+    auto all_seq = new T[all_size];
+    for(int i=0; i<size; ++i) all_seq[i] = sequence[i];
+    for(int i=0; i<this->size; ++i) all_seq[i+size] = set[i];
+    std::sort(all_seq, all_seq + all_size);//O(NlogN) // might have same items
+    // allocate buffer if needed
+    if(all_size > bufferSize) allocateNewBuffer(all_size);
+    int cnt=0;
+    for(int i=0;i<all_size;++i){//O(N)
+        assert(bufferSize>=this->size);
+        cnt+=add(all_seq[i]);
+    }
+    delete[] all_seq;
+    std::sort(set, set+ this->size);//O(NlogN)
+    return cnt > 0;
+}
+
+
+//O(N)
+template<typename T>
+bool MySet<T>::remove(T item){
+    if(!has(item))return false;
+    int ok = size-1, ng = -1;
+    while(ok-ng>1){
+        int mid=(ok+ng)/2;
+        if(set[mid]>=item)ok=mid;
+        else ng=mid;
+    }
+    //remove set[ok]
+    assert(set[ok]==item);
+    // left shift
+    for(int i=ok;i<size-1;++i) set[i] = set[i+1];
+    --size;
+    return true;
+}
+
+
+//O(logN)
+template<typename T>
+bool MySet<T>::has(T item) const{
+    assert(bufferSize>=size);
+    if(size==0 || set[size-1]<item)return false;
+    bool ret = false;
+    // set[ok]>=item is always true
+    int ok=size-1, ng = -1;
+    assert(set[ok]>=item);
+    while(ok-ng>1){
+        int mid = (ok+ng)/2;
+        if(set[mid]>=item)ok=mid;
+        else ng = mid;
+        assert(set[ok]>=item);
+    }
+    return set[ok]==item;
+}
+
+
+//O(N)
+template<typename T>
+MySet<T> MySet<T>::unionWith(const MySet& anotherSet) const{
+    auto ret = MySet(anotherSet);
+    ret.add(this->set, this->size);
+    return ret;
+}
+
+
+//O( NlogN )
+template<typename T>
+MySet<T> MySet<T>::intersectWith(const MySet& anotherSet) const{
+    assert(bufferSize>=size);
+    auto ano_set = anotherSet.set;
+    auto ano_size = anotherSet.size;
+    auto ret = MySet();
+    for (int i = 0; i < ano_size; i++){
+        if(!has(ano_set[i]))continue;//O(logN)
+        ret.add(ano_set[i]);// O(1) cuz ano_set is ascending order
+    }
+    return ret;
+}
+
+
+//O( 1 )
+template<typename T>
+void MySet<T>::clear(){
+    assert(bufferSize>=size);
+    delete[] set;
+    set = new T[10];
+    size=0, bufferSize=10;
+    return;
+}
+
+
+//O( 1 )
+template<typename T>
+int MySet<T>::getSize() const{return size;}
+
+
+//O( N )
+template<typename T>
+void MySet<T>::print() const{
+    assert(bufferSize>=size);
+    std::cout<<"{";
+    for (int i = 0; i < size; i++) std::cout<< set[i]<<", ";
+    std::cout<<"}"<<std::endl;
+}
+
+// This is only for test code
+template<typename T>
+bool MySet<T>::isSameSet(std::set<T>& checkSet)const{{
+    std::set<T> realSet;
+    for (int i = 0; i < size; i++) realSet.insert(set[i]);
+    assert(realSet.size()==size);
+    return realSet == checkSet;
+}}
--- Assignment3/test.cpp
+++ Assignment3/test.cpp
@@ -12,44 +12,46 @@ void test(){
     //single add, remove
     set<int> st;
     MySet<int> mySt;
-    // REP(i,1000){
-    //     int a = rand()%100000000;
-    //     st.insert(a), mySt.add(a);
-    //     a = rand()%100;
-    //     st.erase(a), mySt.remove(a);
-    //     assert(mySt.isSameSet(st));
-    // }
-    // cout<<"..OK!\n";
+    REP(i,1000){
+        int a = rand()%100000000;
+        st.insert(a), mySt.add(a);
+        a = rand()%100;
+        st.erase(a), mySt.remove(a);
+        assert(mySt.isSameSet(st));
+    }
+    cout<<"..OK!\n";
 
 
-    // //bulk add(check first enough)
-    // set<int> stB;
-    // MySet<int> myStB;
-    // int N=1000000;
-    // auto  bb = new int[N]; 
-    // REP(i,N){
-    //     int a = rand()%10000000;
-    //     bb[i]=a;
-    //     stB.insert(a);
-    // }
-    // myStB.add(bb, N);
-    // assert(myStB.isSameSet(stB));
-    // cout<<"..OK!\n";
+    //bulk add(check first enough)
+    set<int> stB;
+    MySet<int> myStB;
+    int N=1000000;
+    auto  bb = new int[N]; 
+    REP(i,N){
+        int a = rand()%10000000;
+        bb[i]=a;
+        stB.insert(a);
+    }
+    myStB.add(bb, N);
+    assert(myStB.isSameSet(stB));
+    cout<<"..OK!\n";
 
-    // //intersect
-    // auto myInterSect = mySt.intersectWith(myStB);
-    // set<int> interSect;
-    // for(auto e:st) if(stB.find(e)!=stB.end())interSect.insert(e);
-    // assert(myInterSect.isSameSet(interSect));
-    // cout<<"..OK!\n";
+    //intersect
+    auto myInterSect = mySt.intersectWith(myStB);
+    set<int> interSect;
+    for(auto e:st) if(stB.find(e)!=stB.end())interSect.insert(e);
+    assert(myInterSect.isSameSet(interSect));
+    cout<<"..OK!\n";
 
-    // //union
-    // auto myUnionSet = mySt.unionWith(myStB);
-    // set<int> unionSet;
-    // for(auto e:st)unionSet.insert(e);
-    // for(auto e:stB)unionSet.insert(e);
-    // assert(myUnionSet.isSameSet(unionSet));
-    // cout<<"..OK!\n";
+    //union
+    auto myUnionSet = mySt.unionWith(myStB);
+    set<int> unionSet;
+    for(auto e:st)unionSet.insert(e);
+    for(auto e:stB)unionSet.insert(e);
+    assert(myUnionSet.isSameSet(unionSet));
+    cout<<"..OK!\n";
+    
+    
 }
 
 int main(){
